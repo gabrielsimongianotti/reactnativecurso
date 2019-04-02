aea9ef98c267a910fdee74da33f08787dbed58b5
gabrielsimongianotti <gabrielgianotti@outlook.com>
2019-04-02 09:40:16 -0300

    conectado a api

--- app4/App.js
+++ app4/App.js
@@ -1,27 +1,28 @@
 
-import React, {Component} from 'react';
-import { View} from 'react-native';
-import ListaItens from './src/components/ListaItens.js';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
 import Itens from './src/components/Itens.js';
 import axios from 'axios';
 
 
 type Props = {};
 export default class App extends Component<Props> {
-
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = { listaItens: [] }
+  }
+  componentWillMount() {
     // requisição HTTP                                                    
     axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
-    //callback com arrow function
-    .then(response => { console.log(response); })
-    .catch(() => { console.log('Erro ao recuperar os dados'); });
+      //callback com arrow function
+      .then(response => { this.setState({ listaItens: response.data }) })
+      .catch(() => { console.log('Erro ao recuperar os dados'); });
   }
   render() {
     return (
       <View>
-        <Itens />
-        <Itens />
-        <Itens />
+        {this.state.listaItens.map(item => (<Itens Key={item.titulo} item={item} />))}
       </View>
     );
   }
--- app4/src/components/Itens.js
+++ app4/src/components/Itens.js
@@ -1,16 +1,20 @@
 
-import React, {Component} from 'react';
-import { Text} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Image } from 'react-native';
 
-export default class Itens extends Component{
+export default class Itens extends Component {
 
   render() {
-    
+
     return (
-        <Text>
-            Detalhes do itens
-        </Text>
-    
+      <View>
+        <Image style={{ height: 100, width: 100 }} source={{ uri:this.props.item.foto }} />
+        <Text>{this.props.item.foto}</Text>
+        <Text>{this.props.item.titulo}</Text>
+        <Text>{this.props.item.valor}</Text>
+        <Text>{this.props.item.local_anuncio}</Text>
+        <Text>{this.props.item.data_publicacao}</Text>
+      </View>
     );
   }
 }
